0c2a8fbeb1152c219d1c8099aa52778dea84fae1
Oscar Ivan Ramirez <oscivaramtin@gmail.com>
2021-01-17 10:19:19 -0500

    Process stats, process execution
    Now the view [ProcessWindows] could execute and show the process stats.

--- ui/ProcessStatistics.py
+++ ui/ProcessStatistics.py
@@ -3,48 +3,57 @@ import numpy as np
 import tkinter as tk
 from tkinter import ttk
 
-"""
-Those mock are for visualization purpose. As soon as possible we have obtain that info (Processor model)
-from controller layer.
-"""
 
-# Mock for Process name
-process_name = ["Process1", "Process2", "Process3", "Process4", "Process5"]
+class ProcessStatistics:
 
-# Mock for process size
-process_size = [40, 50, 100, 200, 50]
+    def __init__(self, process):
 
-# Mock for process threads
-process_threads = np.zeros(5)
+        # Mock for Process name
+        self.process_name = ["Process1", "Process2", "Process3", "Process4", "Process5"]
 
-# Mock for process resources
-# Those process resources should be a type of ENUM
-process_resources = ["1", "2", "3", "4", "5"]
+        # Mock for process size
+        self.process_size = [40, 50, 100, 200, 50]
 
-# Mock for process state
-# Those process resources should be a type of ENUM also
-process_state = ["ready", "execution", "ready", "block", "ready"]
+        # Mock for process threads
+        self.process_threads = np.zeros(5)
 
-root = tk.Tk()
+        # Mock for process resources
+        # Those process resources should be a type of ENUM
+        self.process_resources = ["1", "2", "3", "4", "5"]
 
-process_df = pd.DataFrame({
-    "Process name": process_name,
-    "process size": process_size,
-    "process_threads": process_threads,
-    "process_resources": process_resources,
-    "process state": process_state
-})
+        # Mock for process state
+        # Those process resources should be a type of ENUM also
+        self.process_state = ["ready", "execution", "ready", "block", "ready"]
 
-cols = list(process_df.columns)
+        self.show_process_stats()
 
-tree = ttk.Treeview(root)
-tree.pack()
-tree["columns"] = cols
-for i in cols:
-    tree.column(i, anchor='w')
-    tree.heading(i, text=i, anchor='w')
+    def show_process_stats(self):
 
-for index, row in process_df.iterrows():
-    tree.insert("", 0, text=index, values=list(row))
+        """
+        Those mock are for visualization purpose. As soon as possible we have obtain that info (Processor model)
+        from controller layer.
+        """
 
-root.mainloop()
+        root = tk.Tk()
+
+        process_df = pd.DataFrame({
+            "Process name": self.process_name,
+            "process size": self.process_size,
+            "process_threads": self.process_threads,
+            "process_resources": self.process_resources,
+            "process state": self.process_state
+        })
+
+        cols = list(process_df.columns)
+
+        tree = ttk.Treeview(root)
+        tree.pack()
+        tree["columns"] = cols
+        for i in cols:
+            tree.column(i, anchor='w')
+            tree.heading(i, text=i, anchor='w')
+
+        for index, row in process_df.iterrows():
+            tree.insert("", 0, text=index, values=list(row))
+
+        root.mainloop()
--- ui/ProcessWindow.py
+++ ui/ProcessWindow.py
@@ -1,13 +1,14 @@
-import PySimpleGUI as sg
-
 from tkinter import *
 
 from class_Proceso import Proceso
+from ui.ProcessStatistics import ProcessStatistics
 
 
 class ProcessWindow:
 
     def __init__(self, win):
+        self.process_to_execute = []
+
         self.lb_process_id = Label(win, text="Process id")
         self.lb_process_name = Label(win, text="Name")
         self.lb_process_weight = Label(win, text="Weight")
@@ -19,7 +20,10 @@ class ProcessWindow:
         self.input_process_weight = Entry()
         self.input_process_threads = Entry()
 
-        #TODO("Resources should be treated like a resources predefined list")
+        clicked = StringVar()
+        clicked.set("Resource1")
+        self.resources_menu = OptionMenu(win, clicked, "Resource1", "Resource2", "Resource3", "Resource4", "Resource5")
+        self.resources_menu.pack()
 
         self.lb_process_id.place(x=100, y=50)
         self.input_process_id.place(x=200, y=50)
@@ -29,11 +33,22 @@ class ProcessWindow:
         self.input_process_weight.place(x=200, y=150)
         self.lb_process_threads.place(x=100, y=200)
         self.input_process_threads.place(x=200, y=200)
+        self.lb_process_resources.place(x=100, y=250)
+        self.resources_menu.place(x=200, y=250)
+
+        # Frame for created process
+        self.process_frame = LabelFrame(win, text="Created process", padx=10, pady=10)
+        self.process_frame.pack(padx=10, pady=10)
+        self.process_frame.place(x=350, y=50)
 
         self.btn_create_process = Button(win, text='Accept', command=self.create, width=5, height=1)
         self.btn_new_process = Button(win, text='New', command=self.new)
+        self.btn_execute_process = Button(win, text='Execute', command=self.execute)
+        self.btn_process_stats = Button(win, text='Process stats', command=self.stats)
         self.btn_create_process.place(x=0, y=280)
         self.btn_new_process.place(x=60, y=280)
+        self.btn_execute_process.place(x=120, y=280)
+        self.btn_process_stats.place(x=180, y=280)
 
     def create(self):
         process_id = int(self.input_process_id.get())
@@ -41,15 +56,22 @@ class ProcessWindow:
         process_weight = int(self.input_process_weight.get())
         process_threads = int(self.input_process_threads.get())
 
-        process_characteristics_to_save = [
+        process_detail = [
             process_id,
             process_name,
             process_weight,
             process_threads,
-            "resoruces is pending"
-        ]
+            "resoruces is pending"]
 
-        process = Proceso(process_characteristics_to_save)
+        process = Proceso(process_detail)
+
+        self.process_to_execute.append(process)
+
+        self.add_process_to_table(process_name=process_name)
+
+    def add_process_to_table(self, process_name):
+        process_label = Label(self.process_frame, text=process_name)
+        process_label.pack()
 
     # Clear all fields
     def new(self):
@@ -57,6 +79,22 @@ class ProcessWindow:
         self.input_process_name.delete(0, 'end')
         self.input_process_weight.delete(0, 'end')
         self.input_process_threads.delete(0, 'end')
+
+    def execute(self):
+        # The [Proceso] shall be able to receive a list of process
+        # process = Proceso(self.process_to_execute)
+        self.clean_process_frame()
+
+    def clean_process_frame(self):
+        for widget in self.process_frame.winfo_children():
+            widget.destroy()
+
+        self.new()
+
+    def stats(self):
+        # Call [ProcessStatistics] and pass the process information, we have to get this
+        # information from model [Process]
+        ProcessStatistics(self.process_to_execute)
 
 
 window = Tk()
