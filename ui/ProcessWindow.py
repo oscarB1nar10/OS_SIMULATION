660eb8cf97a5b5d7e5e8f9c2c08e9773b18de2aa
Langeloo <73621548+Langeloo@users.noreply.github.com>
2021-01-25 22:01:05 -0500

    Se han agregado dos archivos de tipo .ICO de 24 pixeles y en ProcessWindow se ha modificado la fincion ejecutar.

--- ui/ProcessWindow.py
+++ ui/ProcessWindow.py
@@ -36,15 +36,15 @@ class ProcessWindow:
         self.resources_menu.pack()
 
         self.lb_process_id.place(x=100, y=50)
-        self.input_process_id.place(x=200, y=50)
+        self.input_process_id.place(x=210, y=50)
         self.lb_process_name.place(x=100, y=100)
-        self.input_process_name.place(x=200, y=100)
+        self.input_process_name.place(x=210, y=100)
         self.lb_process_weight.place(x=100, y=150)
-        self.input_process_weight.place(x=200, y=150)
+        self.input_process_weight.place(x=210, y=150)
         self.lb_process_threads.place(x=100, y=200)
-        self.input_process_threads.place(x=200, y=200)
+        self.input_process_threads.place(x=210, y=200)
         self.lb_process_resources.place(x=100, y=250)
-        self.resources_menu.place(x=200, y=250)
+        self.resources_menu.place(x=210, y=245)
 
         # Frame for created process
         self.process_frame = LabelFrame(win, text="Created process", padx=10, pady=10)
@@ -54,11 +54,12 @@ class ProcessWindow:
         self.btn_create_process = Button(win, text='Accept', command=self.create, width=5, height=1)
         self.btn_new_process = Button(win, text='New', command=self.new)
         self.btn_execute_process = Button(win, text='Execute', command=self.execute)
-        self.btn_process_stats = Button(win, text='Process stats', command=self.stats)
-        self.btn_create_process.place(x=0, y=280)
-        self.btn_new_process.place(x=60, y=280)
-        self.btn_execute_process.place(x=120, y=280)
-        self.btn_process_stats.place(x=180, y=280)
+        self.btn_return_home = Button(win,text='Return',command=self.return_home)
+        #self.btn_process_stats = Button(win, text='Process stats', command=self.stats)
+        self.btn_create_process.place(x=40, y=310)
+        self.btn_new_process.place(x=180, y=310)
+        self.btn_execute_process.place(x=300, y=310)
+        #self.btn_process_stats.place(x=180, y=280)
 
     def create(self):
         process_id = int(self.input_process_id.get())
@@ -106,8 +107,27 @@ class ProcessWindow:
     def execute(self):
         # The [Proceso] shall be able to receive a list of process
         # process = Proceso(self.process_to_execute)
-        self.clean_process_frame()
-
+        self.lb_process_id.destroy()
+        self.lb_process_name.destroy()
+        self.lb_process_id.destroy()
+        self.lb_process_threads.destroy()
+        self.lb_process_weight.destroy()
+        self.lb_process_resources.destroy()
+        self.input_process_id.destroy()
+        self.input_process_weight.destroy()
+        self.input_process_name.destroy()
+        self.input_process_threads.destroy()
+        self.btn_new_process.destroy()
+        #self.btn_process_stats.destroy()
+        self.btn_execute_process.destroy()
+        self.btn_create_process.destroy()
+        self.resources_menu.destroy()
+        self.process_frame.destroy()
+        self.btn_return_home.place(x=10, y=5)
+        window.title('Start Process')
+        window.iconbitmap('boton-de-play.ico')
+        window.geometry('1280x720')
+        #self.clean_process_frame()
         while self.model.hayprocesos():
             process_result = self.model.Corriendo()
             process_stats = ProcessStatistics(process_result, third_frame)
@@ -124,6 +144,8 @@ class ProcessWindow:
         # information from model [Process]
         ProcessStatistics(self.process_to_execute, third_frame)
 
+    def return_home(self):
+        print('No se que hacer o poner aqui')
 
 window = Tk()
 main_frame = Frame(window)
@@ -145,9 +167,10 @@ my_canvas.bind('<Configure>', lambda e: my_canvas.configure(scrollregion=my_canv
 third_frame = Frame(my_canvas)
 
 # Add that new frame to a window in the canvas
-my_canvas.create_window((0, 300), window=third_frame, anchor="nw")
+my_canvas.create_window((40,30), window=third_frame, anchor="nw")
 
 my_win = ProcessWindow(main_frame)
-window.title('Hello Python')
-window.geometry('500x300+10+10')
+window.title('Create Process')
+window.iconbitmap('home_icon.ico')
+window.geometry('500x390')
 window.mainloop()
